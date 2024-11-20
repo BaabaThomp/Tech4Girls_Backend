bee0c400aa3fdd3c007d13168e62b01dcdd449db
BaabaThomp <beatriceabathompson@gmail.com>
2024-11-20 14:43:30 +0000

    car

---
+++ OOP_Concepts/class_basics.py
@@ -0,0 +1,13 @@
+class Car:
+    def__init__(self, make, model, year)
+      self.make = make
+      self.model = model
+      self.year = year
+      
+    def display_info(self):
+      return f"(car details)
+  
+  my_new_car= Car('Toyota,' 'Camry',2021):
+    
+    
+    
